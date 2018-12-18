403b701cdd53689b618e3ddba9d74833544a4fd5
Ryan <ryan.knittel@gmail.com>
2018-12-17 21:52:37 -0800

    updated to .NET 4.7.1 & updated nuget packages.

--- SidebarDiagnostics/Properties/Resources.Designer.cs
+++ SidebarDiagnostics/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SidebarDiagnostics.Framework {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1339,7 +1339,7 @@ namespace SidebarDiagnostics.Framework {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Enables this hardware. (Double click to change. Known bug.).
+        ///   Looks up a localized string similar to Enables this hardware..
         /// </summary>
         public static string SettingsHardwareEnabledTooltip {
             get {
