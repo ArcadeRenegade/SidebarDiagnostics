5cbd3d1478891e280fa9fa2a70a658f73b8552d3
ThaChillera <thachillera@protonmail.com>
2019-12-09 21:13:41 +0100

    retargeted for .net framework 4.7.2

--- SidebarDiagnostics/Properties/Resources.Designer.cs
+++ SidebarDiagnostics/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SidebarDiagnostics.Framework {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1942,7 +1942,7 @@ namespace SidebarDiagnostics.Framework {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Temperatures for sensors and alerts will be in Fahrenheit instead of Celcius..
+        ///   Looks up a localized string similar to Temperatures for sensors and alerts will be in Fahrenheit instead of Celsius..
         /// </summary>
         public static string SettingsUseFahrenheitTooltip {
             get {
