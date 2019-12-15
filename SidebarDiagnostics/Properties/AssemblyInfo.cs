775787950cef68c0366d36e7e31651817c7bd1dc
Robin K <kuiper.robin@gmail.com>
2019-12-15 19:37:24 +0100

    transformed to .net core project, can run (but crashes)

--- SidebarDiagnostics/Properties/AssemblyInfo.cs
+++ SidebarDiagnostics/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sidebar Diagnostics")]
-[assembly: AssemblyDescription("Sidebar Diagnostics")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ArcadeRenegade")]
-[assembly: AssemblyProduct("Sidebar Diagnostics")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -39,17 +33,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page, 
                                               // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.3.0")]
-[assembly: AssemblyFileVersion("3.5.3.0")]
